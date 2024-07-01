00ea9ffa30a46bbde4b84a3ff987085e0f1310c5
Reagandennis <dennis215@gmail.com>
2024-07-01 14:01:31 +0100

    new

--- lib/screens/HomePage.dart
+++ lib/screens/HomePage.dart
@@ -8,20 +8,30 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(30.0),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                "Welcome, $username!",
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Welcome, $username",
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.person, size: 30),
+                    onPressed: () {
+                      // Handle profile icon tap
+                    },
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+          ],
+    ),
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.white,
@@ -48,10 +58,6 @@ class Homepage extends StatelessWidget {
             icon: Icon(Icons.settings),
             label: 'Settings',
             ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
         ],
         showSelectedLabels: true,
         showUnselectedLabels: true,
